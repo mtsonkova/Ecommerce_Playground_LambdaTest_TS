4923d005df9d3c4965f1f7ed7ccf0259ecd429a7
mtsonkova <mtsonkova950@gmail.com>
2025-08-17 15:01:03 +0300

    extracted locators for Register page

---
+++ src/pageLocators/account/registerPageLocators.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+
+export const RegisterPageLocators = (page: Page) => ({
+    firstName: page.locator('#input-firstname'),
+    lastName: page.locator('#input-lastname'),
+    email: page.locator('#input-email'),
+    telephone: page.locator('#input-telephone'),
+    password: page.locator('#input-password'),
+    confirmPassword: page.locator('#input-confirm'),
+    newsletterSunbscribe: {
+        yes: page.locator('#input-newsletter-yes'),
+        no: page.locator('#input-newsletter-no')
+    },
+    privacyPolicyCheckbox: page.locator('#input-agree'),
+    continueBtn: page.locator('input[type="Submit"]'),
+});
