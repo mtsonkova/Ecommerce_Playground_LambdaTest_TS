882b58fdfe193d6aa343beffcbda0ff309f62c08
mtsonkova <mtsonkova950@gmail.com>
2025-08-17 13:44:38 +0300

    Extracted main home page locators

---
+++ src/pageLocators/homePageLocators.ts
@@ -0,0 +1,54 @@
+import {Page} from '@playwright/test';
+
+export const HomePageLocators = (page:Page) => {
+    const searchResults = 'ul.dropdown-menu.autocomplete';
+    const productsActions = 'div.product-action';
+    const menuItems = 'ul.navbar-nav.horizontal';
+
+    return {
+    mainHeader: {
+        searchSection: {
+            allCategories: page.locator('.dropdown.search-category button'),
+            searchField: page.locator('input[name="search"]'),
+            searchBtn: page.getByRole('button', {name: 'Search'}),
+        },
+        resultItems: page.locator(`${searchResults} li`),
+        compareIcon: page.locator('span[data-original-title= "Compare"]'),
+        favoritesIcon: page.locator('span[data-original-title="Wishlist"]'),
+        shoppingCartIcon: page.locator('div.cart-icon:nth-of-type(1) div.icon.svg-icon'),
+        shoppingCartBadge: page.locator('div.cart-icon:nth-of-type(1) span:nth-of-type(1)'),
+        miniCartDrawer: page.locator('div#cart-total-drawer'),
+        miniCartSection: {
+            emptyMiniCart: {
+            subTotal: page.locator('tr:nth-of-type(1) td:nth-of-type(2)'),
+            total: page.locator('tr:nth-of-type(2) td:nth-of-type(2)'),
+            },
+            productsMiniCart: {
+                productsTable: page.locator('div.table-responsive > table.table'),
+            },
+            editCart: page.locator('#entry_217850 a'),
+            checkoutBtn: page.locator('#entry_217851 a'),
+        }
+    },
+
+    mainMenu: {
+        shopByCategory: page.getByText('Shop by Category'),
+        home: page.getByText(' Home'),
+        special: page.getByText(' Special'),
+        blog: page.getByText(' Blog'),
+        megaMenu: page.getByText(' Mega Menu'),
+        addOns: page.getByText(' AddOns'),
+        myAccount: page.getByText(' My account'),
+    },
+
+    productsSection: {
+        productsItems: page.locator('div.carousel-item'),
+        productsActions: {
+            addToCartBtn: page.locator(`${productsActions} button.btn.btn-cart`),
+            addToWishlistbtn: page.locator(`${productsActions} button.btn.btn-wishlist`),
+            quickViewBtn: page.locator(`${productsActions} button.btn.btn-quick-view`),
+            compareBtn: page.locator(`${productsActions} button.btn.btn-compare`),
+        }       
+    }
+};
+};
