980a0881c065dc1d635603339fd8ea1131359a83
mtsonkova <mtsonkova950@gmail.com>
2025-08-19 21:52:08 +0300

    Added order success locators

---
+++ src/pageLocators/checkoutSuccessLocators.ts
@@ -0,0 +1,6 @@
+import { Page } from "@playwright/test";
+
+export const checkoutSuccessLocators = (page: Page) => ({
+    checkoutSuccessTitle: page.locator('h1.page-title'),
+    continueBtn: page.locator('div.buttons a'),
+});
