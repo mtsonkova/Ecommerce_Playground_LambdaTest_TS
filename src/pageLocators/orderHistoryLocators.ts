6bc8ae8da4b3c5cc81afc0917c5bc54717759f44
mtsonkova <mtsonkova950@gmail.com>
2025-08-21 09:41:46 +0300

    chore formatting
    Fixed indentation for orderHistoryLocators.ts file

--- src/pageLocators/orderHistoryLocators.ts
+++ src/pageLocators/orderHistoryLocators.ts
@@ -1,14 +1,14 @@
 import { Page } from "@playwright/test";
 
 export const OrderHistoryLocators = (page: Page) => ({
-orderHistoryTitle: page.locator('h1.page-title.h3'),
-tableRows: page.locator('#content tbody tr'),
-orderId: page.locator('#content tbody tr td').nth(0),
-customer:page.locator('#content tbody tr td').nth(1),
-numOfProducts:page.locator('#content tbody tr td').nth(2),
-status:page.locator('#content tbody tr td').nth(3),
-total:page.locator('#content tbody tr td').nth(4),
-dateAdded:page.locator('#content tbody tr td').nth(5),
-viewOrderBtn:page.locator('#content tbody tr td').nth(6).locator('a'),
-continueBtn: page.locator('div.buttons a'),
+    orderHistoryTitle: page.locator('h1.page-title.h3'),
+    tableRows: page.locator('#content tbody tr'),
+    orderId: page.locator('#content tbody tr td').nth(0),
+    customer:page.locator('#content tbody tr td').nth(1),
+    numOfProducts:page.locator('#content tbody tr td').nth(2),
+    status:page.locator('#content tbody tr td').nth(3),
+    total:page.locator('#content tbody tr td').nth(4),
+    dateAdded:page.locator('#content tbody tr td').nth(5),
+    viewOrderBtn:page.locator('#content tbody tr td').nth(6).locator('a'),
+    continueBtn: page.locator('div.buttons a'),
 });
